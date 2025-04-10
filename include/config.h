ecac97d20800fe11130e217257b76b85cb7bc83a
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2025-04-10 09:43:46 +0200

    refactor, home assistant, arduino json

--- include/config.h
+++ include/config.h
@@ -4,3 +4,6 @@ extern const char *ssid;
 extern const char *password;
 extern const char *deviceName;
 extern const char *hostName;
+
+extern const char *mqttServer;
+extern uint16_t mqttPort;
--- src/main.cpp
+++ src/main.cpp
@@ -3,60 +3,41 @@
 #include <Arduino.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
-
-#include <SoftwareSerial.h>
+#include <PubSubClient.h>
 #include <MHZ.h>
-
 #include <WS2812FX.h>
-
 #include <RecurringTask.h>
+#include <ArduinoJson.h>
 
 #include "config.h"
 #include "util.h"
 
-// CO2 sensor
-const auto MH_Z19_RX = 14;
-const auto MH_Z19_TX = 16;
-// SoftwareSerial swSerial(MH_Z19_RX, MH_Z19_TX);
-MHZ co2(MH_Z19_RX, MH_Z19_TX, MHZ::MHZ19B);
+// === Constants ===
+constexpr uint8_t PIN_MHZ_RX = 14;
+constexpr uint8_t PIN_MHZ_TX = 16;
+
+constexpr uint8_t PIN_LED = 15;
+constexpr uint8_t LED_COUNT = 1;
+
+constexpr int SPEED_SLOW = 5000;
+constexpr int SPEED_FAST = 1000;
+
+constexpr int LEVEL_GOOD = 1000;
+constexpr int LEVEL_WARNING = 1500;
+constexpr int LEVEL_DANGER = 2000;
+
+// === Globals ===
+MHZ co2(PIN_MHZ_RX, PIN_MHZ_TX, MHZ::MHZ19B);
 bool isCalibrated = false;
-// MHZ19 co2;
 
-// WS2812
-#define LED_COUNT 1
-#define LED_PIN 15
-WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+WS2812FX ws2812fx(LED_COUNT, PIN_LED, NEO_GRB + NEO_KHZ800);
 
-#define LEVEL_BASE 400
-#define LEVEL_GOOD 1000
-#define LEVEL_WARNING 1500
-#define LEVEL_DANGER 2000
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
-#define SPEED_SLOW 5000
-#define SPEED_FAST 1000
+String deviceId = String("co2mon_") + ESP.getChipId();
 
-/**
- *
- */
-void onOTAStart()
-{
-    ws2812fx.setBrightness(5);
-    ws2812fx.setColor(rgb(0, 0, 255));
-    ws2812fx.setMode(FX_MODE_STATIC);
-    ws2812fx.service();
-}
-
-/**
- *
- */
-void onOTAEnd()
-{
-    ws2812fx.setBrightness(25);
-    ws2812fx.setColor(rgb(0, 0, 255));
-    ws2812fx.setMode(FX_MODE_STATIC);
-    ws2812fx.service();
-}
-
+// === LED Utils ===
 inline void setBrightness(uint8_t brightness)
 {
     static uint8_t prev = 0;
@@ -85,123 +66,126 @@ inline void setMode(uint8_t mode)
                  { ws2812fx.setMode(m); });
 }
 
-/**
- *
- * @brief
- *
- */
-void setup()
+// === OTA ===
+void setupOTA()
 {
-    Serial.begin(115200);
-    delay(100);
-    Serial.println(deviceName);
+    ArduinoOTA.setHostname(hostName);
+    ArduinoOTA.onStart([]()
+                       {
+        ws2812fx.setBrightness(5);
+        ws2812fx.setColor(rgb(0, 0, 255));
+        ws2812fx.setMode(FX_MODE_STATIC);
+        ws2812fx.service(); });
+    ArduinoOTA.onEnd([]()
+                     {
+        ws2812fx.setBrightness(25);
+        ws2812fx.setColor(rgb(0, 0, 255));
+        ws2812fx.setMode(FX_MODE_STATIC);
+        ws2812fx.service(); });
+    ArduinoOTA.begin();
+}
 
-    //
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    //
-    co2.setAutoCalibrate(false);
-    co2.setBypassCheck(true, true);
-    co2.setDebug(false);
-
-    // Indicator setup
-    ws2812fx.init();
-    ws2812fx.start();
-
-    setBrightness(15);
-    setColor(rgb(255, 255, 255));
-    setMode(FX_MODE_BREATH);
-    setSpeed(SPEED_FAST);
-
-    //
+// === WiFi ===
+void connectWiFi()
+{
     WiFi.mode(WIFI_STA);
-    WiFi.setHostname(deviceName);
+    WiFi.setHostname(hostName);
     WiFi.begin(ssid, password);
+
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(10);
         ws2812fx.service();
     }
-    Serial1.println("WiFi connected");
-
-    setBrightness(5);
-    setColor(rgb(0, 0, 255));
-    setMode(FX_MODE_BREATH);
-    setSpeed(SPEED_SLOW);
-
-    //
-    MDNS.begin(hostName);
-
-    //
-    ArduinoOTA.setHostname(hostName);
-    ArduinoOTA.onStart([]()
-                       { onOTAStart(); });
-    ArduinoOTA.onEnd([]()
-                     { onOTAEnd(); });
-    ArduinoOTA.begin();
+    Serial.println("WiFi connected");
 }
 
-/**
- *
- */
-void measureCO2()
+// === MQTT ===
+String buildDiscoveryPayload()
 {
-    if (co2.isPreHeating())
+    StaticJsonDocument<512> doc;
+
+    doc["name"] = "CO2";
+    doc["state_topic"] = "sensors/mhz19b/co2";
+    doc["unit_of_measurement"] = "ppm";
+    doc["device_class"] = "carbon_dioxide";
+    doc["state_class"] = "measurement";
+    doc["expire_after"] = 120;
+    doc["unique_id"] = "co2-" + String(ESP.getChipId());
+
+    JsonObject device = doc.createNestedObject("device");
+    device["identifiers"][0] = deviceId;
+    device["name"] = deviceName;
+    device["manufacturer"] = "DIY";
+    device["model"] = "MH-Z19B CO2 Sensor";
+    device["sw_version"] = "1.0";
+
+    String output;
+    serializeJson(doc, output);
+    return output;
+}
+
+void reconnectMQTT()
+{
+    while (!mqttClient.connected())
     {
-        setBrightness(5);
-        setColor(rgb(0, 192, 255));
-        setMode(FX_MODE_STATIC);
-        // Serial.println("Preheating");
-        return;
+        Serial.print("Attempting MQTT connection...");
+
+        if (mqttClient.connect("MHZ19B_Client"))
+        {
+            Serial.println("connected");
+
+            String payload = buildDiscoveryPayload();
+            mqttClient.publish("homeassistant/sensor/mhz19b/config", payload.c_str(), true);
+        }
+        else
+        {
+            Serial.printf("failed, rc=%d. Retrying in 5s...\n", mqttClient.state());
+            delay(5000);
+        }
     }
+}
 
-    digitalWrite(LED_BUILTIN, LOW);
+void publishCO2Data(int ppm)
+{
+    mqttClient.publish("sensors/mhz19b/co2", String(ppm).c_str(), true);
+}
 
-    int ppm_uart = co2.readCO2UART();
+void publishTemperatureData(float temperature)
+{
+    mqttClient.publish("sensors/mhz19b/temperature", String(temperature).c_str(), true);
+}
 
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    Serial.print("PPMuart: ");
-    if (ppm_uart > 0)
-    {
-        Serial.println(ppm_uart);
-    }
-    else
-    {
-        Serial.println("n/a");
-    }
-
-    if (ppm_uart <= LEVEL_GOOD)
+// === CO2 Reading ===
+void updateLedEffect(int ppm)
+{
+    if (ppm <= LEVEL_GOOD)
     {
         setBrightness(5);
         setColor(rgb(0, 255, 0));
-        setSpeed(5000);
         setMode(FX_MODE_STATIC);
+        setSpeed(SPEED_SLOW);
     }
-    else if (ppm_uart <= LEVEL_WARNING)
+    else if (ppm <= LEVEL_WARNING)
     {
         setBrightness(5);
 
-        uint32_t r = transform(ppm_uart, range(LEVEL_GOOD, LEVEL_WARNING), range(0, 255));
+        uint32_t r = transform(ppm, range(LEVEL_GOOD, LEVEL_WARNING), range(0, 255));
         setColor(rgb(r, 255, 0));
 
-        uint16_t speed = transform(ppm_uart, range(LEVEL_GOOD, LEVEL_DANGER), range(SPEED_FAST, SPEED_SLOW));
+        uint16_t speed = transform(ppm, range(LEVEL_GOOD, LEVEL_DANGER), range(SPEED_FAST, SPEED_SLOW));
         setSpeed(speed);
-
         setMode(FX_MODE_BREATH);
     }
-    else if (ppm_uart <= LEVEL_DANGER)
+    else if (ppm <= LEVEL_DANGER)
     {
-        uint8_t brightness = transform(ppm_uart, range(LEVEL_WARNING, LEVEL_DANGER), range(5, 20));
+        uint8_t brightness = transform(ppm, range(LEVEL_WARNING, LEVEL_DANGER), range(5, 20));
+        uint32_t g = transform(ppm, range(LEVEL_WARNING, LEVEL_DANGER), range(0, 255));
+        uint16_t speed = transform(ppm, range(LEVEL_GOOD, LEVEL_DANGER), range(SPEED_FAST, SPEED_SLOW));
+
         setBrightness(brightness);
-
-        uint32_t g = transform(ppm_uart, range(LEVEL_WARNING, LEVEL_DANGER), range(0, 255));
         setColor(rgb(255, g, 0));
-
-        uint16_t speed = transform(ppm_uart, range(LEVEL_GOOD, LEVEL_DANGER), range(SPEED_FAST, SPEED_SLOW));
         setSpeed(speed);
-
         setMode(FX_MODE_BREATH);
     }
     else
@@ -213,34 +197,87 @@ void measureCO2()
     }
 }
 
-void calibrate()
+void measureCO2()
 {
-    unsigned long now = millis();
-    const unsigned long waitUntilCalibration = TIME_TO_MS(0, 30, 0);
+    if (co2.isPreHeating())
+    {
+        setBrightness(5);
+        setColor(rgb(0, 192, 255));
+        setMode(FX_MODE_STATIC);
+        return;
+    }
 
-    if (!isCalibrated && now > waitUntilCalibration)
+    digitalWrite(LED_BUILTIN, LOW);
+    int ppm = co2.readCO2UART();
+    float temperature = co2.getLastTemperature();
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    Serial.print("PPMuart: ");
+    Serial.println(ppm > 0 ? String(ppm) : "n/a");
+
+    if (ppm > 0)
+    {
+        updateLedEffect(ppm);
+        publishCO2Data(ppm);
+    }
+}
+
+// === Optional: Calibration ===
+void calibrateSensor()
+{
+    const unsigned long waitMs = TIME_TO_MS(0, 30, 0);
+    if (!isCalibrated && millis() > waitMs)
     {
         co2.calibrateZero();
         isCalibrated = true;
     }
 }
 
-/**
- * @brief
- *
- */
+// === Setup ===
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    co2.setAutoCalibrate(false);
+    co2.setBypassCheck(true, true);
+    co2.setDebug(false);
+
+    ws2812fx.init();
+    ws2812fx.start();
+
+    setBrightness(15);
+    setColor(rgb(255, 255, 255));
+    setMode(FX_MODE_BREATH);
+    setSpeed(SPEED_FAST);
+
+    connectWiFi();
+    setBrightness(5);
+    setColor(rgb(0, 0, 255));
+    setMode(FX_MODE_BREATH);
+    setSpeed(SPEED_SLOW);
+
+    MDNS.begin(deviceName);
+    setupOTA();
+
+    mqttClient.setServer(mqttServer, mqttPort);
+}
+
+// === Main loop ===
 void loop()
 {
+    if (!mqttClient.connected())
+    {
+        reconnectMQTT();
+    }
+    mqttClient.loop();
     ws2812fx.service();
-
     ArduinoOTA.handle();
 
     RecurringTask::interval(10000, []()
                             { measureCO2(); });
+    // RecurringTask::interval(1000, []() { calibrateSensor(); });
 
-    // RecurringTask::interval(1000, []()
-    //                         { calibrate(); });
-
-    // relax a bit
     delay(1);
 }
